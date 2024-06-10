ec46525013dbfb3112b3154ffc7649f5a7a2d0e5
Manishtmg <127338906+Manishtmg@users.noreply.github.com>
2024-06-09 23:55:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function navigate(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.log('Error:', error));
+}
