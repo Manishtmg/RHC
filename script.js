e688bad5fe09a3347fad6782d12d509f4819bc45
Manishtmg <127338906+Manishtmg@users.noreply.github.com>
2024-06-26 16:17:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function navigate(page) {
-    window.location.href = page;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
