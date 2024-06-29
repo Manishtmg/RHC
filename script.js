85c3b35610f7b0a9e81408b5323f05c3c3a2ecc8
Manishtmg <127338906+Manishtmg@users.noreply.github.com>
2024-06-29 16:18:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdown = document.querySelector('.dropdown');
 
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('show');
+    });
+
+    dropdownToggle.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default link behavior
+        dropdown.classList.toggle('show');
     });
 });
